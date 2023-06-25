6f7a1e5adcf461a474fae25608068d55799bd64e
Grasath <misteridler06@gmail.com>
2023-06-25 23:36:18 +0530

    Update Database

--- database.js
+++ database.js
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
     host : process.env.mysql_host,
     user : process.env.mysql_user,
     password : process.env.mysql_password,
-    database : process.env.mysql_database
+    database : process.env.mysql_database,
+    connectTimeout: 30000
 }).promise();
 
 const main = async ()=>{
